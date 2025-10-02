53ce6e7d370ff718950223c4d7a556a28c120921
decio2022 <113923924+decio2022@users.noreply.github.com>
2025-10-02 20:10:55 -0300

    fix

--- js/layers.js
+++ js/layers.js
@@ -170,35 +170,8 @@ addLayer("megamulti", {
     color: "#4BDC13",
     requires: new Decimal(25), // Can be a function that takes requirement increases into account
     resource: "Mega Multiplier", // Name of prestige currency
-    baseResource: "Prestige", // Name of resource prestige is based on
-    baseAmount() {return player.prestige.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: Decimal.reciprocate("10^^1e300"), // Prestige currency exponent
-    effect() {
-        let eff = new Decimal(1)
-        eff = player.megamulti.points.mul(2).max(1)
-        return eff
-    },
-    effectDescription() {
-        return " which boost Ultra Prestige and Rebirth by x" + tmp.megamulti.effect
-    },
-    row: 5, // Row the layer is in on the tree (0 is the first row)
-    layerShown(){return true},
-})
-
-addLayer("megamulti", {
-    name: "Mega Multiplier", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "MM", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "#4BDC13",
-    requires: new Decimal(25), // Can be a function that takes requirement increases into account
-    resource: "Mega Multiplier", // Name of prestige currency
-    baseResource: "Prestige", // Name of resource prestige is based on
-    baseAmount() {return player.prestige.points}, // Get the current amount of baseResource
+    baseResource: "Ultra Prestige", // Name of resource prestige is based on
+    baseAmount() {return player.uprestige.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: Decimal.reciprocate("10^^1e300"), // Prestige currency exponent
     effect() {
