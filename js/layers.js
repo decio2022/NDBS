487e6749577fa8238cc75bebb639388112d24e35
decio2022 <113923924+decio2022@users.noreply.github.com>
2025-10-02 19:07:08 -0300

    Added Rebirth and Ultra Rebirth stats

--- js/layers.js
+++ js/layers.js
@@ -15,7 +15,7 @@ addLayer("multi", {
     exponent: Decimal.reciprocate("10^^1e300"), // Prestige currency exponent
     effect() {
         let eff = new Decimal(1)
-        eff = player.multi.points.pow_base(2)
+        eff = player.multi.points.times(2).max(1)
         return eff
     },
     effectDescription() {
@@ -23,8 +23,58 @@ addLayer("multi", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     layerShown(){return true},
-    doReset(resettingLayer) {
-        let keep = [];
-        if (layers[resettingLayer].row > this.row) layerDataReset("multi", keep)
+})
+
+addLayer("rebirth", {
+    name: "Rebirth", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(25), // Can be a function that takes requirement increases into account
+    resource: "Rebirth", // Name of prestige currency
+    baseResource: "Multiplier", // Name of resource prestige is based on
+    baseAmount() {return player.multi}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: Decimal.reciprocate("10^^1e300"), // Prestige currency exponent
+    effect() {
+        let eff = new Decimal(1)
+        eff = player.rebirth.points.times(2).max(1)
+        return eff
     },
+    effectDescription() {
+        return " which boost Multiplier by x" + tmp.rebirth.effect
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true},
+})
+
+addLayer("urebirth", {
+    name: "Ultra Rebirth", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "UR", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(25), // Can be a function that takes requirement increases into account
+    resource: "Ultra Rebirth", // Name of prestige currency
+    baseResource: "Rebirth", // Name of resource prestige is based on
+    baseAmount() {return player.rebirth}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: Decimal.reciprocate("10^^1e300"), // Prestige currency exponent
+    effect() {
+        let eff = new Decimal(1)
+        eff = player.urebirth.points.times(2).max(1)
+        return eff
+    },
+    effectDescription() {
+        return " which boost Rebirth by x" + tmp.urebirth.effect
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true},
 })
