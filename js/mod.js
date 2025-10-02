db098415e930113c216da9258a2e04d0168360a5
decio2022 <113923924+decio2022@users.noreply.github.com>
2025-10-02 19:41:16 -0300

    Changed mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "New Difficulty Button Simulator Remake",
+	author: "decio2022",
+	pointsName: "money",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
